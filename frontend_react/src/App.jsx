0277878a9590a7e7b9bf3db9078e6fb4073783d0
Anubhav <anubhavpal15june@gmail.com>
2023-06-04 15:26:19 +0530

    update

--- frontend_react/src/App.jsx
+++ frontend_react/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import './app.scss'
 import { Footer, Skills, Work, Testimonial, About, Header } from './containers/index.js'
 import { Navbar } from './components/index.js'
 
